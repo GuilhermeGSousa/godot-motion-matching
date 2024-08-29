cef2f5d99b50b71f4451d5d830533690f5c6b837
Guilherme Sousa <guilherme.sousa1994@gmail.com>
2024-08-29 09:36:52 +0200

    Add collisions to trajectory generation This also reworks the player movement model to use critically damped springs instead

--- src/mm_trajectory_point.h
+++ src/mm_trajectory_point.h
@@ -7,8 +7,25 @@
 
 using namespace godot;
 
+struct MMCollisionState {
+    bool on_floor = false;
+    Vector3 floor_normal;
+    Vector3 floor_position;
+    bool against_wall = false;
+    Vector3 wall_normal;
+};
+
 struct MMTrajectoryPoint {
     Vector3 position;
     Vector3 velocity;
     float facing_angle;
+
+    MMCollisionState collision_state;
+
+    Transform3D get_transform(const Vector3& up_axis = Vector3(0, 1, 0)) const {
+        Transform3D result;
+        result.origin = position;
+        result.set_basis(Basis(up_axis, facing_angle));
+        return result;
+    }
 };
