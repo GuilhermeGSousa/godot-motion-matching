c59e019ddff9cf914117249c58f0d1a95d1c85c1
Guilherme Sousa <guilherme.sousa1994@gmail.com>
2024-09-12 11:01:11 +0200

    Fix player controller script

--- demo/scripts/player_controller.gd
+++ demo/scripts/player_controller.gd
@@ -2,9 +2,10 @@ extends Node
 
 @export var max_speed = 3.0
 @export var character : MMCharacter
+@export var camera_pivot: Node3D
 
 func _physics_process(delta: float) -> void:
 	var stick_input = Input.get_vector("left", "right", "down", "up")
 	var stick_input_world = Vector3(-stick_input.x, 0, stick_input.y)
-	
-	character.target_velocity = stick_input_world * max_speed
+	var desired_velocity = (stick_input_world * max_speed).rotated(Vector3.UP, camera_pivot.rotation.y)
+	character.target_velocity = desired_velocity
