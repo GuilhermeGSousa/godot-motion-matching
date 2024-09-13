4987cbb92abfa452452080573346e7283d15c3ca
Guilherme Sousa <guilherme.sousa1994@gmail.com>
2024-09-13 10:16:34 +0200

    Debug table UI improvements

--- demo/scripts/debug_table.gd
+++ demo/scripts/debug_table.gd
@@ -1,12 +1,11 @@
 extends Control
 
-@export var motion_matcher : MMCharacter
-@export var title_scene : PackedScene
-@export var cell_scene : PackedScene
+@export var motion_matcher: MMCharacter
+@export var title_scene: PackedScene
+@export var cell_scene: PackedScene
+@export var table : GridContainer
 
-@onready var table = $Panel/TableContainer
-
-var cells : Array[TableCell] = []
+var cells: Array[TableCell] = []
 
 func _ready() -> void:
 	for c in table.get_children():
@@ -15,7 +14,7 @@ func _ready() -> void:
 	table.columns = 4
 	motion_matcher.on_query_result.connect(_on_query_result)
 	
-func _on_query_result(data : Dictionary):
+func _on_query_result(data: Dictionary):
 
 	for c in cells:
 		c.queue_free()
@@ -24,23 +23,26 @@ func _on_query_result(data : Dictionary):
 	table.columns = len(data.keys()) - 1
 	
 	for key in data.keys():
-		if(key == "frame_data"):
+		if (key == "frame_data"):
 			continue
 		add_title(key)
 		
 	for val in data.values():
-		if(val is PackedFloat32Array):
+		if (val is PackedFloat32Array):
 			continue
-		add_cell(str(val))
+		var val_str = str(val)
+		if val_str.contains("/"):
+			val_str = val_str.split("/")[1]
+		add_cell(val_str)
 
-func add_title(data : String):
-	var cell : TableCell = title_scene.instantiate() as TableCell
+func add_title(data: String):
+	var cell: TableCell = title_scene.instantiate() as TableCell
 	cell.set_data(data)
 	table.add_child(cell)
 	cells.append(cell)
 
-func add_cell(data : String):
-	var cell : TableCell = cell_scene.instantiate() as TableCell
+func add_cell(data: String):
+	var cell: TableCell = cell_scene.instantiate() as TableCell
 	cell.set_data(data)
 	table.add_child(cell)
 	cells.append(cell)
