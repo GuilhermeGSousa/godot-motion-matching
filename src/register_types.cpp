3c02756f0a6b0d2aa3ee7ef87c944b20a31f6b39
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-10-25 14:19:32 -0700

    Rename  to motion_matching_module

--- src/register_types.cpp
+++ src/register_types.cpp
@@ -28,7 +28,7 @@
 
 using namespace godot;
 
-void initialize_example_module(ModuleInitializationLevel p_level) {
+void initialize_motion_matching_module(ModuleInitializationLevel p_level) {
     if (p_level == MODULE_INITIALIZATION_LEVEL_EDITOR) {
         ClassDB::register_internal_class<MMEditorGizmoPlugin>();
         ClassDB::register_internal_class<MMEditor>();
@@ -54,7 +54,7 @@ void initialize_example_module(ModuleInitializationLevel p_level) {
     }
 }
 
-void uninitialize_example_module(ModuleInitializationLevel p_level) {
+void uninitialize_motion_matching_module(ModuleInitializationLevel p_level) {
 }
 
 extern "C" {
@@ -62,8 +62,8 @@ extern "C" {
 GDExtensionBool GDE_EXPORT example_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization* r_initialization) {
     godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
 
-    init_obj.register_initializer(initialize_example_module);
-    init_obj.register_terminator(uninitialize_example_module);
+    init_obj.register_initializer(initialize_motion_matching_module);
+    init_obj.register_terminator(uninitialize_motion_matching_module);
     init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SERVERS);
 
     return init_obj.init();
--- src/register_types.h
+++ src/register_types.h
@@ -4,5 +4,5 @@
 
 using namespace godot;
 
-void initialize_example_module(ModuleInitializationLevel p_level);
-void uninitialize_example_module(ModuleInitializationLevel p_level);
+void initialize_motion_matching_module(ModuleInitializationLevel p_level);
+void uninitialize_motion_matching_module(ModuleInitializationLevel p_level);
