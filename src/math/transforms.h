b51cbb9350e958e991a1073d796ef0bab6517f68
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-10-26 06:37:50 -0700

    Fix error spam.

--- src/math/transforms.h
+++ src/math/transforms.h
@@ -37,7 +37,7 @@ float global_to_local_facing_angle(float local_facing_angle, const Transform3D& 
 
     const Vector3 global_dir(cos(local_facing_angle), 0.f, sin(local_facing_angle));
 
-    const Vector3 local_dir = transform.get_basis().get_quaternion().xform(global_dir);
+    const Vector3 local_dir = transform.get_basis().get_rotation_quaternion().xform(global_dir);
 
     return atan2(local_dir.z, local_dir.x);
 }
