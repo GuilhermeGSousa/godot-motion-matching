c86a5a878b164974134d3405ca61efa597070cfd
Guilherme Sousa <guilherme.sousa1994@gmail.com>
2024-10-26 15:28:19 +0200

    Move demo to separate repo

--- demo/scripts/pause_manager.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("pause"):
-		get_tree().paused = !get_tree().paused
--- demo/scripts/table_cell.gd
+++
@@ -1,4 +0,0 @@
-class_name TableCell extends PanelContainer
-
-func set_data(data : String):
-	$Data.text = data
