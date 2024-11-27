f9ea0412f7d2c7fa6e3911065793fc4cf676bc64
Guilherme Sousa <guilherme.sousa1994@gmail.com>
2024-11-27 22:50:10 +0100

    Refactor MMSychronizers (#70)
    * Refactor MMSychronizers
    
    * Added Mix Synchronizer

--- src/synchronizers/mm_rootmotion_synchronizer.cpp
+++ src/synchronizers/mm_rootmotion_synchronizer.cpp
@@ -2,15 +2,20 @@
 
 #include "mm_character.h"
 
-MMSyncResult MMRootMotionSynchronizer::sync(const MMCharacter* controller, const Node3D* character, float delta_time) {
-    MMSyncResult result;
+void MMRootMotionSynchronizer::sync(MMCharacter* p_controller, Node3D* p_character, float p_delta_time) {
+    const Vector3 position_delta = p_character->get_global_position() - p_controller->get_global_position();
+    Vector3 controller_position = p_controller->get_global_position();
+    Vector3 character_position = p_character->get_global_position();
 
-    result.character_position = character->get_global_position();
-    result.character_rotation = character->get_global_basis().get_rotation_quaternion();
-    result.controller_position = character->get_global_position();
-    result.controller_rotation = character->get_global_basis().get_rotation_quaternion();
+    controller_position.x = p_character->get_global_position().x;
+    controller_position.z = p_character->get_global_position().z;
 
-    return result;
+    character_position.y = p_controller->get_global_position().y;
+
+    p_controller->set_global_position(controller_position);
+    p_controller->set_global_basis(p_character->get_global_basis());
+
+    p_character->set_global_position(character_position);
 }
 void MMRootMotionSynchronizer::_bind_methods() {
 }
